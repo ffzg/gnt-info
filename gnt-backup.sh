b392cd18bad8e411a4e39816628d876ef49ab749
Dobrica Pavlinusic <dpavlin@rot13.org>
2015-03-01 13:44:04 +0100

    show instance info before start of export

--- gnt-backup.sh
+++ gnt-backup.sh
@@ -9,6 +9,8 @@ if [ -z "$instance" ] ; then
 	exit 0
 fi
 
+
+gnt-instance list -o name,status,oper_vcpus,oper_ram,disk_usage,pnode,snodes $instance
 gnt-backup export --noshutdown -n $node $instance || true # ignore error on swap partition
 ssh $node zfs snap ${ganeti_export}@`date +%Y-%m-%d`_${instance}
 
