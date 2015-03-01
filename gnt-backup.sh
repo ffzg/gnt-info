c8662b06c219cd8b3700aea76b35fd6db667ba4b
Dobrica Pavlinusic <dpavlin@rot13.org>
2015-03-01 15:21:49 +0100

    support more than one instance as argument

--- gnt-backup.sh
+++ gnt-backup.sh
@@ -1,9 +1,11 @@
 #!/bin/sh -xe
 
-instance=$1
 node=arh-hw
 ganeti_export=arh-hw/ganeti/export
 
+while true ; do
+
+instance=$1
 if [ -z "$instance" ] ; then
 	ssh $node zfs list -t all -r $ganeti_export
 	exit 0
@@ -14,4 +16,6 @@ gnt-instance list -o name,status,oper_vcpus,oper_ram,disk_usage,pnode,snodes $in
 gnt-backup export --noshutdown -n $node $instance || true # ignore error on swap partition
 ssh $node zfs snap ${ganeti_export}@`date +%Y-%m-%d`_${instance}
 
+shift
 
+done
