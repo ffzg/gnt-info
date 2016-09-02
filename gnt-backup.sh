8f8d7d3be0768d5e3bb6cf9585bc8db032bdbec5
Dobrica Pavlinusic <dpavlin@rot13.org>
2016-09-02 21:34:50 +0200

    use nfs

--- gnt-backup.sh
+++ gnt-backup.sh
@@ -1,21 +1,26 @@
 #!/bin/sh -xe
 
-node=arh-hw
-ganeti_export=arh-hw/ganeti/export
+node=`hostname -s`
+zfs_nfs=lib10
+ganeti_export=lib10/arh-hw/ganeti/export
 
 while true ; do
 
-instance=$1
-if [ -z "$instance" ] ; then
-	ssh $node zfs list -o name,written,compressratio -t snapshot -r $ganeti_export
+if [ -z "$1" ] ; then
+	ssh root@$zfs_nfs zfs list -o name,written,compressratio -t snapshot -r $ganeti_export
 	exit 0
 fi
 
+mount | grep /var/lib/ganeti/export || mount $zfs_nfs:/var/lib/ganeti/export /var/lib/ganeti/export/
+
+instance=$1
 
 gnt-instance list -o name,status,oper_vcpus,oper_ram,disk_usage,pnode,snodes $instance
 gnt-backup export --noshutdown -n $node $instance || true # ignore error on swap partition
-ssh $node zfs snap ${ganeti_export}@`date +%Y-%m-%d`_${instance}
+ssh root@$zfs_nfs zfs snap ${ganeti_export}@`date +%Y-%m-%d`_${instance}
 
 shift
 
 done
+
+umount /var/lib/ganeti/export/
