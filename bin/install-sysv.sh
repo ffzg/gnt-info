3eafa557c854cb9e2736b0d72e10f9ccc21657f9
Dobrica Pavlinusic <dpavlin@rot13.org>
2016-12-08 21:02:55 +0100

    sysv init now really works

--- bin/install-sysv.sh
+++ bin/install-sysv.sh
@@ -1,12 +1,15 @@
 #!/bin/sh -xe
-sudo cp gnt-monitor /etc/ganeti/
+sudo cp -v gnt-monitor /etc/ganeti/
+sudo gnt-cluster copyfile /etc/ganeti/gnt-monitor
+
+test -e /etc/default/gnt-monitor || cp -v default/gnt-monitor /etc/default/ && echo "# modify config here"
 sudo gnt-cluster copyfile /etc/default/gnt-monitor
 
 sudo cp init.d/gnt-monitor /etc/init.d/gnt-monitor
 sudo gnt-cluster copyfile /etc/init.d/gnt-monitor
 
 #sudo gnt-cluster command apt-get install psmisc
-sudo gnt-cluster command killall gnt-monitor
+#sudo gnt-cluster command killall -r gnt-monitor
 
 sudo gnt-cluster command service gnt-monitor restart
-sudo gnt-cluster command service gnt-monitor status
+sudo gnt-cluster command 'ps ax | grep gnt-monitor'
