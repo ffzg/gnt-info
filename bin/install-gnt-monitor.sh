f96a87660a796120bde8f055e5e2f44c7135a0bd
Dobrica Pavlinusic <dpavlin@rot13.org>
2016-12-08 10:09:08 +0100

    reload systemd config, kill running gnt-monitors

--- bin/install-gnt-monitor.sh
+++ bin/install-gnt-monitor.sh
@@ -3,6 +3,8 @@ sudo cp gnt-monitor /etc/ganeti/
 sudo gnt-cluster copyfile /etc/ganeti/gnt-monitor
 sudo cp systemd/gnt-monitor.service /etc/systemd/system/
 sudo gnt-cluster copyfile /etc/systemd/system/gnt-monitor.service
+sudo gnt-cluster command systemctl daemon-reload
 sudo gnt-cluster command systemctl enable gnt-monitor
+sudo gnt-cluster command killall gnt-monitor
 sudo gnt-cluster command systemctl start gnt-monitor
 sudo gnt-cluster command systemctl status gnt-monitor
