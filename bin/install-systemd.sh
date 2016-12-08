758fbb88c90ebc3dd49d93d19a6506c757745ea5
Dobrica Pavlinusic <dpavlin@rot13.org>
2016-12-08 22:50:03 +0100

    systemd default file

--- bin/install-systemd.sh
+++ bin/install-systemd.sh
@@ -1,10 +1,15 @@
 #!/bin/sh -xe
 sudo cp gnt-monitor /etc/ganeti/
 sudo gnt-cluster copyfile /etc/ganeti/gnt-monitor
+
 sudo cp systemd/gnt-monitor.service /etc/systemd/system/
 sudo gnt-cluster copyfile /etc/systemd/system/gnt-monitor.service
+
+test -e /etc/default/gnt-monitor || cp -v default/gnt-monitor /etc/default/ && echo "# modify config here"
+sudo gnt-cluster copyfile /etc/default/gnt-monitor
+
 sudo gnt-cluster command systemctl daemon-reload
 sudo gnt-cluster command systemctl enable gnt-monitor
-sudo gnt-cluster command killall gnt-monitor
+#sudo gnt-cluster command killall gnt-monitor
 sudo gnt-cluster command systemctl start gnt-monitor
 sudo gnt-cluster command systemctl status gnt-monitor
