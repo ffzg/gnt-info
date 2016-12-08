ddb991e7700e32f1777a8832c290d98f1f30da61
Dobrica Pavlinusic <dpavlin@rot13.org>
2016-12-08 09:35:06 +0100

    install gnt-monitor using systemd

---
+++ bin/install-gnt-monitor.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -xe
+sudo cp gnt-monitor /etc/ganeti/
+sudo gnt-cluster copyfile /etc/ganeti/gnt-monitor
+sudo cp systemd/gnt-monitor.service /etc/systemd/system/
+sudo gnt-cluster copyfile /etc/systemd/system/gnt-monitor.service
+sudo gnt-cluster command systemctl enable gnt-monitor
+sudo gnt-cluster command systemctl start gnt-monitor
+sudo gnt-cluster command systemctl status gnt-monitor
