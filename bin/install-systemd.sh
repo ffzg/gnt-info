5ad7cb081c85d4b85aa4592d453c9d93c345eccc
Dobrica Pavlinusic <dpavlin@rot13.org>
2016-12-09 10:40:59 +0100

    stop/start gnt-monitor using systemd

--- bin/install-systemd.sh
+++ bin/install-systemd.sh
@@ -11,5 +11,6 @@ sudo gnt-cluster copyfile /etc/default/gnt-monitor
 sudo gnt-cluster command systemctl daemon-reload
 sudo gnt-cluster command systemctl enable gnt-monitor
 #sudo gnt-cluster command killall gnt-monitor
+sudo gnt-cluster command systemctl stop gnt-monitor
 sudo gnt-cluster command systemctl start gnt-monitor
 sudo gnt-cluster command systemctl status gnt-monitor
