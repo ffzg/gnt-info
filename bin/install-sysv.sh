322a58dc9230970a212a8fff73b17a19c62e99dc
Dobrica Pavlinusic <dpavlin@rot13.org>
2016-12-09 19:32:58 +0100

    document bug and add workaround

--- bin/install-sysv.sh
+++ bin/install-sysv.sh
@@ -9,7 +9,7 @@ sudo cp init.d/gnt-monitor /etc/init.d/gnt-monitor
 sudo gnt-cluster copyfile /etc/init.d/gnt-monitor
 
 #sudo gnt-cluster command apt-get install psmisc
-#sudo gnt-cluster command killall -r gnt-monitor
+sudo gnt-cluster command killall -r gnt-monitor
 
 sudo gnt-cluster command service gnt-monitor restart
 sudo gnt-cluster command 'ps ax | grep gnt-monitor'
