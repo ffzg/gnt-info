b3f2396ac053339567765514e890ecd2a980788a
Dobrica Pavlinusic <dpavlin@rot13.org>
2016-12-23 18:32:13 +0100

    few careful shell escapes, and now it works

--- gnt-lv-snap-rsync.sh
+++ gnt-lv-snap-rsync.sh
@@ -18,15 +18,20 @@ ssh $node lvs -o name,tags | grep $instance | tee /dev/shm/$instace.$node.lvs | 
 	echo "# $lv | $origin | $disk_nr"
 
 cat <<__SHELL__ | tee /dev/shm/$instance.sh
+
 	lvcreate -L20480m -s -n$lv.snap /dev/ffzgvg/$lv
+
 	mkdir /dev/shm/$lv.snap
+
 	# we must mount filesystem read-write to allow journal recovery
 	mount /dev/ffzgvg/$lv.snap /dev/shm/$lv.snap -o noatime \
-	|| offset=`fdisk -l /dev/ffzgvg/$lv.snap -u | grep Linux$ | grep /dev/ffzgvg/$lv.snap | head -1 | awk '{ print $2 * 512 }'` \
-	&& mount /dev/ffzgvg/$lv.snap /dev/shm/$lv.snap -o noatime,offset=$offset \
+	|| offset=\`fdisk -l /dev/ffzgvg/$lv.snap -u | grep Linux$ | grep /dev/ffzgvg/$lv.snap | head -1 | awk '{ print \$2 * 512 }'\` \
+	&& mount /dev/ffzgvg/$lv.snap /dev/shm/$lv.snap -o noatime,offset=\$offset \
 
 	rsync -ravHz --numeric-ids --sparse --delete /dev/shm/$lv.snap/ lib15::backup/$instance/$disk_nr/
+
 	umount /dev/shm/$lv.snap
+
 	lvremove -f /dev/ffzgvg/$lv.snap
 __SHELL__
 
