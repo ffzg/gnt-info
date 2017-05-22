9bd644636297cc4d378d136399ce86ea423a7634
Dobrica Pavlinusic <dpavlin@rot13.org>
2017-05-22 21:04:08 +0200

    don't exit if wathed job failed

--- gnt-running.sh
+++ gnt-running.sh
@@ -3,7 +3,7 @@ while true ; do
   JOBID=`gnt-job list --no-headers --running | tee /dev/stderr | awk '{ print $1; exit }'`
   if [ -n "$JOBID" ]
   then
-	gnt-job watch $JOBID
+	gnt-job watch $JOBID || true # don't exit on failed jobs
   else
 	sleep 5
   fi
