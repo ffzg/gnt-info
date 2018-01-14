01d6bd55fe58f933d06a9c67c3cc7fb35cd780ae
Dobrica Pavlinusic <dpavlin@rot13.org>
2018-01-14 13:55:46 +0100

    execute SMART smartctl params on each drive
    e.g. SMART="-t long" ./smart-megaraid.sh

--- smart-megaraid.sh
+++ smart-megaraid.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Usage:
+# smart-megaraid.sh '^# 1'		# default without args
+# SMART="-t long" smart-megaraid.sh	# execute smart command
+
 pattern='^# 1'
 test ! -z "$1" && pattern=$1
 
@@ -7,6 +11,7 @@ megaraid() {
 	drive=$1
 	nr=0
 	while [ $nr -lt 8 ] ; do
+		test ! -z "$SMART" && smartctl -d megaraid,$nr $SMART /dev/$drive > /dev/shm/smart.$drive.$nr-out
 		smartctl -d megaraid,$nr -a /dev/$drive > /dev/shm/smart.$drive.$nr
 		nr=`expr $nr + 1`
 	done
@@ -15,6 +20,7 @@ megaraid() {
 test -r /proc/mdstat && cat /proc/mdstat
 
 lsblk --noheadings --scsi -o name | while read drive ; do
+	test ! -z "$SMART" && smartctl $SMART /dev/$drive > /dev/shm/smart.$drive-out
 	smartctl -a /dev/$drive > /dev/shm/smart.$drive
 	if ! grep -q '^# 1' /dev/shm/smart.$drive ; then
 		megaraid $drive
