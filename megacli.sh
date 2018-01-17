7785e972136db639cb75b61e1f23249f958e39e3
Dobrica Pavlinusic <dpavlin@rot13.org>
2018-01-17 19:18:39 +0100

    LSI controller boot log

---
+++ megacli.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ "$1" = "install" ] ; then
+
+	cat >/dev/apt/sources.list.d/hwraid.list << __SOURCES__
+# wget -O - https://hwraid.le-vert.net/debian/hwraid.le-vert.net.gpg.key | sudo apt-key add -
+deb http://hwraid.le-vert.net/debian stretch main
+__SOURCES__
+	wget -O - https://hwraid.le-vert.net/debian/hwraid.le-vert.net.gpg.key | sudo apt-key add -
+	apt-get update
+	apt-get install megacli
+fi
+
+megacli -FwTermLog Dsply -aALL | tee /dev/shm/megacli.FwTermLog | egrep -v '(Sense|IllegalReq|pRdm)'
+
