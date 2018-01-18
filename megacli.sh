4a8495f50210afdc2242ae05d24732ad515196b8
Dobrica Pavlinusic <dpavlin@rot13.org>
2018-01-18 11:52:05 +0100

    fix hwraid.list path

--- megacli.sh
+++ megacli.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" = "install" ] ; then
 
-	cat >/dev/apt/sources.list.d/hwraid.list << __SOURCES__
+	cat >/etc/apt/sources.list.d/hwraid.list << __SOURCES__
 # wget -O - https://hwraid.le-vert.net/debian/hwraid.le-vert.net.gpg.key | sudo apt-key add -
 deb http://hwraid.le-vert.net/debian stretch main
 __SOURCES__
