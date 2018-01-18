f104e00746345dd70982e5b3403f04f367d8a2e6
Dobrica Pavlinusic <dpavlin@rot13.org>
2018-01-18 11:44:58 +0100

    cleanup log output

--- megacli.sh
+++ megacli.sh
@@ -9,7 +9,15 @@ __SOURCES__
 	wget -O - https://hwraid.le-vert.net/debian/hwraid.le-vert.net.gpg.key | sudo apt-key add -
 	apt-get update
 	apt-get install megacli
+
+elif [ ! -z "$1" ] ; then
+	megacli -$* -aALL | tee /dev/shm/megacli.$1
+	exit 0
 fi
 
-megacli -FwTermLog Dsply -aALL | tee /dev/shm/megacli.FwTermLog | egrep -v '(Sense|IllegalReq|Rdm|Unknown DCDB Opcode)'
+megacli -AdpAllInfo -aALL | tee /dev/shm/megacli.AdpAllInfo
+
+megacli -LdPdInfo -aALL | tee /dev/shm/megacli.AdpAllInfo
+
+megacli -AdpAlILog -aALL | tee /dev/shm/megacli.AdpAlILog | egrep -v '(Sense|IllegalReq|Rdm|Unknown DCDB Opcode)'
 
