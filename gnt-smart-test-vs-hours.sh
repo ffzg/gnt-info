d0f5c611000e864cdb4fd207ab81fe410e66548d
Dobrica Pavlinusic <dpavlin@rot13.org>
2018-01-19 12:04:23 +0100

    added serial, use cache output in /dev/shm

--- gnt-smart-test-vs-hours.sh
+++ gnt-smart-test-vs-hours.sh
@@ -1,6 +1,8 @@
-#!/bin/sh -xe
+#!/bin/sh -e
 
-test -e /tmp/smart || sudo ./gnt-smart.sh > /tmp/smart
-egrep 'Power_On_Hours' /tmp/smart | sed 's/: /:/' > /tmp/smart.2
-egrep '# 1' /tmp/smart | sed 's/: /:/' > /tmp/smart.1
-join /tmp/smart.1 /tmp/smart.2 | sed 's/ [0-9]* Power_On_Hours.*Always - / ~ /'
+test -e /dev/shm/gnt-smart || sudo ./gnt-smart.sh
+egrep 'Power_On_Hours' /dev/shm/gnt-smart | sed 's/: /:/' > /dev/shm/gnt-smart.2
+egrep '# 1' /dev/shm/gnt-smart | sed 's/: /:/' > /dev/shm/gnt-smart.1
+egrep 'Serial' /dev/shm/gnt-smart | sed 's/: /:/' > /dev/shm/gnt-smart.3
+join /dev/shm/gnt-smart.1 /dev/shm/gnt-smart.2 | sed 's/ [0-9]* Power_On_Hours.*Always - / ~ /' > /dev/shm/gnt-smart.12
+join /dev/shm/gnt-smart.12 /dev/shm/gnt-smart.3 | sed -e 's/ *- *~/ -\t~/' -e 's/ *~ */\t/' -e 's/Serial.*:/\t/' | tee /dev/shm/gnt-smart.out
