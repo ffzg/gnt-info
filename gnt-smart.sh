a0bd4927e65364791998cd31fe304bccbb966bb8
Dobrica Pavlinusic <dpavlin@rot13.org>
2018-01-19 12:03:59 +0100

    create cache output in /dev/shm/gnt-smart

--- gnt-smart.sh
+++ gnt-smart.sh
@@ -2,4 +2,4 @@
 
 cp /srv/gnt-info/smart-megaraid.sh /tmp/
 gnt-cluster copyfile /tmp/smart-megaraid.sh
-gnt-cluster command -M /tmp/smart-megaraid.sh $*
+gnt-cluster command -M /tmp/smart-megaraid.sh $* | tee /dev/shm/gnt-smart
