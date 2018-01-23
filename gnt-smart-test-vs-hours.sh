f8bc2b6f1475189cc2f8e08468a3155f1716f96d
Dobrica Pavlinusic <dpavlin@rot13.org>
2018-01-23 16:08:52 +0100

    report drives without tests run on them

--- gnt-smart-test-vs-hours.sh
+++ gnt-smart-test-vs-hours.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -e
 
 test -e /dev/shm/gnt-smart || sudo ./gnt-smart.sh
-egrep 'Power_On_Hours' /dev/shm/gnt-smart | sed 's/: /:/' > /dev/shm/gnt-smart.2
-egrep '# 1' /dev/shm/gnt-smart | sed 's/: /:/' > /dev/shm/gnt-smart.1
-egrep 'Serial' /dev/shm/gnt-smart | sed 's/: /:/' > /dev/shm/gnt-smart.3
-join /dev/shm/gnt-smart.1 /dev/shm/gnt-smart.2 | sed 's/ [0-9]* Power_On_Hours.*Always - / ~ /' > /dev/shm/gnt-smart.12
-join /dev/shm/gnt-smart.12 /dev/shm/gnt-smart.3 | sed -e 's/ *- *~/ -\t~/' -e 's/ *~ */\t/' -e 's/Serial.*:/\t/' | tee /dev/shm/gnt-smart.out
+egrep '# 1' /dev/shm/gnt-smart | sed 's/: /:/' | sort > /dev/shm/gnt-smart.1
+egrep 'Power_On_Hours' /dev/shm/gnt-smart | sed 's/: /:/' | sort > /dev/shm/gnt-smart.2
+egrep 'Serial' /dev/shm/gnt-smart | sed 's/: /:/' | sort > /dev/shm/gnt-smart.3
+join -a 2 /dev/shm/gnt-smart.1 /dev/shm/gnt-smart.2 | sed 's/ [0-9]* Power_On_Hours.*Always - / ~ /' > /dev/shm/gnt-smart.12
+join -a 1 /dev/shm/gnt-smart.12 /dev/shm/gnt-smart.3 | sed -e 's/ *- *~/ -\t~/' -e 's/ *~ */\t/' -e 's/Serial.*:/\t/' | tee /dev/shm/gnt-smart.out
