c55e95b411a8053e7cd2a1de756205abc2fc6c91
Dobrica Pavlinusic <dpavlin@rot13.org>
2018-01-23 16:01:05 +0100

    clean argument purges /dev/shm/*smart* cache files

--- gnt-smart.sh
+++ gnt-smart.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -e
 
+if [ "$1" = "clean" ] ; then
+	gnt-cluster command -M rm -v '/dev/shm/*smart*'
+fi
+
 cp /srv/gnt-info/smart-megaraid.sh /tmp/
 gnt-cluster copyfile /tmp/smart-megaraid.sh
 gnt-cluster command -M /tmp/smart-megaraid.sh $* | tee /dev/shm/gnt-smart
