007b33aa24c097d098d499e7c03f5460b2eec964
Dobrica Pavlinusic <dpavlin@rot13.org>
2018-01-27 17:56:08 +0100

    added load count to diganose firmware errors

--- gnt-smart-test-vs-hours.sh
+++ gnt-smart-test-vs-hours.sh
@@ -5,4 +5,6 @@ egrep '# 1' /dev/shm/gnt-smart | sed 's/: /:/' | sort > /dev/shm/gnt-smart.1
 egrep 'Power_On_Hours' /dev/shm/gnt-smart | sed 's/: /:/' | sort > /dev/shm/gnt-smart.2
 egrep 'Serial' /dev/shm/gnt-smart | sed 's/: /:/' | sort > /dev/shm/gnt-smart.3
 join -a 2 /dev/shm/gnt-smart.1 /dev/shm/gnt-smart.2 | sed 's/ [0-9]* Power_On_Hours.*Always - / ~ /' > /dev/shm/gnt-smart.12
-join -a 1 /dev/shm/gnt-smart.12 /dev/shm/gnt-smart.3 | sed -e 's/ *- *~/ -\t~/' -e 's/ *~ */\t/' -e 's/Serial.*:/\t/' | tee /dev/shm/gnt-smart.out
+join -a 1 /dev/shm/gnt-smart.12 /dev/shm/gnt-smart.3 | sed -e 's/ *- *~/ -\t~/' -e 's/ *~ */\t/' -e 's/Serial.*:/\t/' > /dev/shm/gnt-smart.123
+egrep 'Load' /dev/shm/gnt-smart | sed 's/: /:/' | awk '{ print $1 " " $11 }' | sort > /dev/shm/gnt-smart.4
+join -a 1 /dev/shm/gnt-smart.123 /dev/shm/gnt-smart.4 | tee /dev/shm/gnt-smart.1234
