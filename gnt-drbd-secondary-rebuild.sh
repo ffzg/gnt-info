425943cdf0aec14761cc400775f61a96cd05da06
Dobrica Pavlinusic <dpavlin@rot13.org>
2018-01-27 17:53:49 +0100

    rebuild broken sencoday node drbd
    This is really ugly, and requires instance reboot, but it allows
    you to resize your disk array and do single transfer. YMMV

---
+++ gnt-drbd-secondary-rebuild.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -xe
+
+test -z "$1" && echo "Usage: $0 node" && exit 1
+node=$1
+
+gnt-node list-drbd --no-headers $node | grep secondary | awk '{ print $3 }' | while read instance ; do
+	echo "## $instance";
+	gnt-instance stop $instance
+	gnt-instance modify -t plain $instance
+	gnt-instance modify -t drbd -n $node --no-wait-for-sync $instance
+	gnt-instance start $instance
+done
+
