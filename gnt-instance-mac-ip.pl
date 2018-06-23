04f59295e0a46a8317fac7276cd701ea67cbe7ac
Dobrica Pavlinusic <dpavlin@rot13.org>
2018-06-23 16:00:38 +0200

    use /dev/shm/mac-ip-vlan from enesej to find instance ip

---
+++ gnt-instance-mac-ip.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use autodie;
+
+my $mac2ip_vlan;
+my ($in,$out);
+open(my $in, '<', '/dev/shm/mac-ip-vlan');
+while(<$in>) {
+	print $out $_ if $out;
+	chomp;
+	my ( $mac, $ip, $vlan ) = split(/\s/,$_);
+	$mac2ip_vlan->{$mac} = [ $ip, $vlan ];
+}
+
+open(my $gnt, '-|', qq{gnt-instance list --no-header --separator=' ' -o name,nic.macs,nic.bridges});
+while(<$gnt>) {
+	chomp;
+	my ( $name,$macs,$bridges ) = split(/\s/,$_);
+	my @macs = split(/,/,$macs);
+	my @br  = split(/,/,$bridges);
+	for my $i ( 0 .. $#macs ) {
+		my $mac = $macs[$i];
+		print "$name $mac $br[$i] ";
+		if ( exists $mac2ip_vlan->{$mac} ) {
+			print join(' ', @{ $mac2ip_vlan->{$mac} });
+		} else {
+			print "? ?";
+		}
+		print "\n";
+	}
+}
