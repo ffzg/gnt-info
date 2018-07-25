b2ee7c63d8988cd2d287108e576f53b37f1fd2b2
Dobrica Pavlinusic <dpavlin@rot13.org>
2018-07-25 16:00:52 +0200

    create /dev/shm/instance, fix domain

--- gnt-vnc.sh
+++ gnt-vnc.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 
-gnt-instance info $1 | grep 'console connection:' | tr -d '()' | tr ':' ' ' | awk '{ print "ssh -L "$6":"$5":"$6" "$8"\nvncviewer localhost:"$10 }'
+gnt-instance info $1 | tee /dev/shm/$1 | grep 'console connection:' | tr -d '()' | tr ':' ' ' | awk '{ print "ssh -L "$6":"$5":"$6" "$8"\nvncviewer localhost:"$10 }' | sed "s/\.gnt\.ffzg\.hr/.net.ffzg.hr/"
 
