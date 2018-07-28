e1958b527a27c5aaa0d2f130dbdc15a092b3c48e
Dobrica Pavlinusic <dpavlin@rot13.org>
2018-07-28 09:43:48 +0200

    format image snapshots in copy/paste friendly format

--- gnt-rbd.sh
+++ gnt-rbd.sh
@@ -11,6 +11,6 @@ done
 
 grep --with-filename logical_id /dev/shm/instance/* | sed "s/^.*\///; s/:.*, '/ /; s/'.*$//;" | tee /dev/shm/instance-rbd | while read instance disk ; do
 	echo "$instance $disk"
-	rbd snap ls $disk | sed "s/^/# /"
+	rbd snap ls $disk | grep -v SNAPID | awk -v instance=$instance -v disk=$disk '{ print instance,disk"@"$2,"#"$1,$3,$4 }'
 done
 
