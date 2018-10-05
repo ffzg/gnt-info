0ac7fabcf4a3b91241022f9688070426c0b4b3b3
Dobrica Pavlinusic <dpavlin@rot13.org>
2018-10-05 09:20:38 +0200

    commit to etckeeper

--- install-debootstrap-4k-msdos.sh
+++ install-debootstrap-4k-msdos.sh
@@ -1,10 +1,12 @@
-#!/bin/sh -xe
+#!/bin/sh -e
 
 if [ ! -e /etc/ganeti/os/debootstrap/common.sh ] ; then
 	mkdir -pv /etc/ganeti/os/debootstrap
 	cp -pv /usr/share/ganeti/os/debootstrap/common.sh /etc/ganeti/os/debootstrap/
 	ln -sfv /etc/ganeti/os/debootstrap/common.sh /usr/share/ganeti/os/debootstrap/common.sh
-	read -p "Overwrite file and copied to /etc/, press enter to patch" wait_for_enter
+	cd /etc/
+	etckeeper commit 'overwrite /usr/share/ganeti/os/debootstrap/common.sh'
 	cd /etc/ganeti/os/debootstrap/
 	cat /srv/gnt-info/doc/deboostrap-4k-msdos.diff | sed 's/common.sh.in/common.sh/' | patch -p1
+	etckeeper commit 'patched os/debootstrap/common.sh for 4k sectors'
 fi
