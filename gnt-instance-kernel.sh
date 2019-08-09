3e57260c6ba8e6e9d54e6359a6e8a81813d7d3e8
Dobrica Pavlinusic <dpavlin@rot13.org>
2019-08-09 15:18:05 +0200

    keep old eth0 names for interfaces on 4.9 kernel

--- gnt-instance-kernel.sh
+++ gnt-instance-kernel.sh
@@ -1,8 +1,16 @@
 #!/bin/sh -xe
 
-KERNEL=3.16
+test -z "$KERNEL" || KERNEL=4.9
 #KERNEL=3.10
 #KERNEL=3.2
 
-gnt-instance modify -H initrd_path=/boot/initrd.img-$KERNEL-kvmU,kernel_path=/boot/vmlinuz-$KERNEL-kvmU $1
+ver=$( ls /boot/config*-4.9* | sort --field-separator="-" --key=5 --reverse | head -1 | cut -d- -f2- )
+
+tar cfvpz /tmp/$ver.tar.gz /lib/modules/$ver
+ls -al /tmp/$ver.tar.gz
+
+#gnt-instance modify -H initrd_path=/boot/initrd.img-$ver,kernel_path=/boot/vmlinuz-$ver $1
+#gnt-instance modify -H kernel_args="ro net.ifnames=0 biosdevname=0" influx # keep old eth0 names
+gnt-instance modify -H initrd_path=/boot/initrd.img-$ver,kernel_path=/boot/vmlinuz-$ver,kernel_args="ro net.ifnames=0 biosdevname=0" $1
+
 
