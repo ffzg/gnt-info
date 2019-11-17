6f62860270c13195f115c420aeb142bf6b080a92
Dobrica Pavlinusic <dpavlin@rot13.org>
2019-11-17 10:06:16 +0100

    check /proc/drbd and correlate with instances

---
+++ gnt-drbd-check.sh
@@ -0,0 +1,10 @@
+
+gnt-cluster command -M ls -l /var/run/ganeti/instance-disks/ | awk '{ print $1 $12 " " $10 }' | tr ':' ' ' | tee /dev/shm/node.drbd.instance.disk
+
+gnt-cluster command -M cat /proc/drbd | grep cs: | grep -v Connected | tee /dev/shm/drbd.check | \
+	sed 's/: / /g' | \
+	while read node drbd status ; do
+		echo "$node $drbd [$status]"
+		grep "$node.*drbd$drbd " /dev/shm/node.drbd.instance.disk || echo "ERROR: can't find $node $drbd"
+	done
+
