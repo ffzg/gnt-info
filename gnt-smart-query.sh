8608f6ed59de0755a589497862312c16185d00c3
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-02-17 20:25:58 +0100

    remove real ganeti chatter

--- gnt-smart-query.sh
+++ gnt-smart-query.sh
@@ -2,4 +2,4 @@
 
 cp smart-query.sh /tmp/smart-query.sh
 gnt-cluster copyfile /tmp/smart-query.sh
-gnt-cluster command /tmp/smart-query.sh $*
+gnt-cluster command -M /tmp/smart-query.sh $* | grep -v '^return'
