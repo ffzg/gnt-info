cb1a19947bc1c40b6da9cda5b37111c5ad353d79
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-02-17 18:33:00 +0100

    added summary by disk model

--- gnt-disks.sh
+++ gnt-disks.sh
@@ -1,3 +1,6 @@
 #!/bin/sh -xe
 
 gnt-cluster command -M lsblk --scsi -m | grep -v '^return' | tee /dev/shm/gnt-disks | grep -v cdrom
+
+gnt-cluster command -M lsblk --scsi --output SIZE,MODEL --noheadings | grep -v '^return' | grep -v '^----*$' | tee /dev/shm/gnt-disk-size-model | cut -d: -f2- | sort | uniq -c
+
