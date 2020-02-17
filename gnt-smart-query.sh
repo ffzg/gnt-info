b0f6266cd9e5eaa6b41fb563632a62564db61296
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-02-17 21:38:09 +0100

    replace space with dot, eg: Serial Number -> Serial.Number

--- gnt-smart-query.sh
+++ gnt-smart-query.sh
@@ -2,8 +2,12 @@
 
 cp smart-query.sh /tmp/smart-query.sh
 gnt-cluster copyfile /tmp/smart-query.sh
+
+# in column names, replace space with dot, eg:
+# Serial Number -> Serial.Number
+
 gnt-cluster command -M \
-	/tmp/smart-query.sh Power_On_Hours Reallocated_Sector_Ct $* \
+	/tmp/smart-query.sh Power_On_Hours Reallocated_Sector_Ct Serial.Number $@ \
 | grep -v '^return' | grep -v '^--*$' \
 | tee /dev/shm/gnt-smart-query | column -t -s :
 
