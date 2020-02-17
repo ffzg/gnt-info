29ac3d632ad9f723aac518eda3dd0e4e1f8ba1e9
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-02-17 18:36:55 +0100

    sort gnt-disks-models by size

--- gnt-disks.sh
+++ gnt-disks.sh
@@ -2,5 +2,5 @@
 
 gnt-cluster command -M lsblk --scsi -m | grep -v '^return' | tee /dev/shm/gnt-disks | grep -v cdrom
 
-gnt-cluster command -M lsblk --scsi --output SIZE,MODEL --noheadings | grep -v '^return' | grep -v '^----*$' | tee /dev/shm/gnt-disk-size-model | cut -d: -f2- | sort | uniq -c
+gnt-cluster command -M lsblk --scsi --output SIZE,MODEL --noheadings | grep -v '^return' | grep -v '^----*$' | tee /dev/shm/gnt-disk-size-model | cut -d: -f2- | sort | uniq -c | sort -k2 -n | tee /dev/shm/gnt-disks-models
 
