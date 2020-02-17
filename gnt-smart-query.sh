23a5ae75360e9cee5f9b6ba9bcfdc750e6ef354e
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-02-17 21:04:39 +0100

    add Power_On_Hours Reallocated_Sector_Ct

--- gnt-smart-query.sh
+++ gnt-smart-query.sh
@@ -2,4 +2,8 @@
 
 cp smart-query.sh /tmp/smart-query.sh
 gnt-cluster copyfile /tmp/smart-query.sh
-gnt-cluster command -M /tmp/smart-query.sh $* | grep -v '^return' | grep -v '^--*$' | tee /dev/shm/gnt-smart-query | column -t -s :
+gnt-cluster command -M \
+	/tmp/smart-query.sh Power_On_Hours Reallocated_Sector_Ct $* \
+| grep -v '^return' | grep -v '^--*$' \
+| tee /dev/shm/gnt-smart-query | column -t -s :
+
