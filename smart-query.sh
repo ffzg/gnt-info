f748a188a5f5526abd201b070b8af9960205acce
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-02-17 21:04:52 +0100

    relaxed glob now that gnt-smart doesn't leave garbage for megaraid

--- smart-query.sh
+++ smart-query.sh
@@ -5,7 +5,7 @@ test -e /tmp/s.last && rm /tmp/s.last
 for col in "Device Model" "User Capacity" $*
 do
 	# sed and sort is required for join later
-	grep "$col" /dev/shm/smart.sd? | sed \
+	grep "$col" /dev/shm/smart.sd* | sed \
 		-e "s/$col: */ : /" \
 		-e "s/[0-9]* $col .* \([0-9][0-9]*\)$/ : \1/" \
 	| sort > /tmp/s.this
