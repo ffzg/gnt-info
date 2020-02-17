ddf6b4eaf319315cb1798bd850412ae35adb7451
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-02-17 18:06:21 +0100

    keep headers, don't display invalid zeros

--- gnt-vg-usage.sh
+++ gnt-vg-usage.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -xe
 
-gnt-cluster command -M vgs --units g --noheadings | grep -v '^return' | awk '{ print $0 " " $7 - $8 }' | tee /dev/shm/gnt-vgs-usage
+gnt-cluster command -M vgs --units g | grep -v '^return' | awk '{ if ( $7 - $8 != 0 ) print $0 " " $7 - $8 ; else print $0 ; }' | tee /dev/shm/gnt-vgs-usage
 
