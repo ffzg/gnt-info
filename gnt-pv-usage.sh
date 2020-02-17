0ef451470bc9a108b6836e05abaeec44a3adec75
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-02-17 18:07:53 +0100

    keep headings

--- gnt-pv-usage.sh
+++ gnt-pv-usage.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -xe
 
-gnt-cluster command -M pvs --units t --noheadings | grep -v '^return' | awk '{ print $0 " " $6 - $7 }' | tee /dev/shm/gnt-pv-usage
+gnt-cluster command -M pvs --units t | grep -v '^return' | awk '{ if ( $6 - $7 != 0 ) print $0 " " $6 - $7 ; else print $0 ; }' | tee /dev/shm/gnt-pv-usage
 
