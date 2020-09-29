5f3175cd22fe9d3ceb1bc8eb277e91753922d7be
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-09-29 19:22:43 +0200

    use find instead of ls to support 100,000 of files

--- gnt-job-purge-older-than.sh
+++ gnt-job-purge-older-than.sh
@@ -4,6 +4,6 @@ job=$1
 
 test -z "$job" && echo "Usage: $0 job_id" && exit 1
 
-ls /var/lib/ganeti/queue/job-* | sed 's/^.*job-//'  | awk -vjob=$job '{ if ( $1 < job ) print "/var/lib/ganeti/queue/job-"$1 }' | xargs echo rm -v
+find /var/lib/ganeti/queue/ -maxdepth 1 -name 'job-*' | sed 's/^.*job-//'  | awk -vjob=$job '{ if ( $1 < job ) print "/var/lib/ganeti/queue/job-"$1 }' | xargs --max-args=10 echo rm -v
 
 
