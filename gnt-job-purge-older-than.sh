1a2e18b8d9e14c4f9ec813839564cdea5acd1ae6
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-09-29 18:48:44 +0200

    fix awk job number

--- gnt-job-purge-older-than.sh
+++ gnt-job-purge-older-than.sh
@@ -4,6 +4,6 @@ job=$1
 
 test -z "$job" && echo "Usage: $0 job_id" && exit 1
 
-ls /var/lib/ganeti/queue/job-* | sed 's/^.*job-//'  | awk '{ if ( $1 < 3194118 ) print "/var/lib/ganeti/queue/job-"$1 }' | xargs rm -v
+ls /var/lib/ganeti/queue/job-* | sed 's/^.*job-//'  | awk -vjob=$job '{ if ( $1 < job ) print "/var/lib/ganeti/queue/job-"$1 }' | xargs echo rm -v
 
 
